6b8f9ebd782a03ec346eefbaa78f8adc0c181858
Paul Dubs <paul.dubs@gmail.com>
2017-10-30 17:47:26 +0100

    Initial Commit

---
+++ src/main/java/tech/dubs/App.java
@@ -0,0 +1,15 @@
+package tech.dubs;
+
+import org.nd4j.linalg.factory.Nd4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(Nd4j.rand(3,3) );
+    }
+}
